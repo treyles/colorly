e114869d6133ef4f041b6da27690fe5710170840
Troy Reyles <troyreyles@gmail.com>
2018-08-01 23:55:23 -0700

    finshed homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,6 @@
 import React from 'react';
+import Icon from '../utils/Icon';
 import { googleAuth } from '../utils/base';
-// import { Link } from 'react-router-dom';
-// import base from '../utils/base';
 
 /* eslint-disable */
 export default class Home extends React.Component {
@@ -13,11 +12,40 @@ export default class Home extends React.Component {
 
   render() {
     return (
-      <button onClick={() => this.props.signInUser(googleAuth)}>
-        Sign In
-      </button>
+      <div className="home">
+        <div className="hero-text-container">
+          <div className="content">
+            <h1>Colorly</h1>
+            <span className="logo">
+              <Icon icon="logo" />
+            </span>
+            <h3 className="pitch">
+              A simple color extraction tool. Save custom palettes to your
+              collection along with the source image for future reference!
+            </h3>
+            <div className="buttons">
+              <button className="twitter-login">
+                Connect with Twitter
+              </button>
+              <button
+                className="google-login"
+                onClick={() => this.props.signInUser(googleAuth)}
+              >
+                Connect with Google
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="hero-image" />
+      </div>
     );
   }
 }
 
 // .then(result => console.log(result.additionalUserInfo.isNewUser));
+
+{
+  /* <button onClick={() => this.props.signInUser(googleAuth)}>
+        Sign In
+      </button> */
+}
